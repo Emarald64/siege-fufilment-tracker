d7e7cd0874d234af9c64375af1628cba6d4ac610
Xanderath <alexgiller@proton.me>
2025-12-20 23:32:16 -0500

    Display all projects

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 var slackID="U0785D5VDEK"
 const siegeUserApiUrl="https://siege.hackclub.com/api/public-beta/user/"
 
+function setSlackID(value){
+    console.log("set slack id to: "+value)
+    if (value){
+        slackID=value
+        localStorage.setItem("slackID",slackID)
+        console.log('removed id asker after input')
+        document.getElementById("slackIDGetter").remove()
+        loadSiegeStuff()
+    }
+}
 
 function getSiegeUserData(){
     return fetch(siegeUserApiUrl+slackID)
-      .then(response => {
+    .then(response => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok');
         }
         return response.json();
-      })
-      .then(data => {
+    })
+    .then(data => {
         return data
-      })
-      .catch(error => {
+    })
+    .catch(error => {
         console.error('Error:', error);
-      });
+    });
 }
 
 async function getSiegeUserDataOrCached(forceRefresh=false){
-    let storedData=localStorage.getItem("SiegeUserData")
+    let storedData=JSON.parse(localStorage.getItem("SiegeUserData"))
     if (forceRefresh || storedData==null || Date.now()>storedData.time+1800000){
         // get new data
         let newData=await (getSiegeUserData())
@@ -27,10 +37,38 @@ async function getSiegeUserDataOrCached(forceRefresh=false){
         localStorage.setItem("SiegeUserData",JSON.stringify({time:Date.now(), data:newData}))
         return newData
     }else{
-        // use cached data
+    // use cached data
         console.log('using cached')
         return storedData.data
     }
 }
 
-console.log(getSiegeUserDataOrCached(true))
+function loadSiegeStuff(){
+    getSiegeUserDataOrCached()
+        .then(data=>{
+            console.log(data)
+            console.log(data.id)
+            const siegeProjects=document.getElementById("siegeProjects")
+            siegeProjects.style.display='grid'
+            for (projectIdx in data.projects){
+                let project=data.projects[projectIdx]
+                const projectBox=document.createElement("div")
+                projectBox.classList.add('projectBox')
+                projectBox.innerHTML=
+                `
+                <h3>${project.name}</h3>
+                `
+                siegeProjects.appendChild(projectBox)
+            }
+        })
+}
+
+function onload(){
+    slackID=localStorage.getItem("slackID")
+    if (slackID){
+        //dont ask if id is known
+        console.log('removed id asker')
+        document.getElementById("slackIDGetter").remove()
+        loadSiegeStuff()
+    }
+}
