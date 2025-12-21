f2a6699c23feded043de0f2b36a0cd8558822b57
Xanderath <alexgiller@proton.me>
2025-12-21 01:06:23 -0500

    show project desription

--- script.js
+++ script.js
@@ -12,55 +12,125 @@ function setSlackID(value){
     }
 }
 
-function getSiegeUserData(){
-    return fetch(siegeUserApiUrl+slackID)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
+// function getSiegeUserData(){
+//     return fetch(siegeUserApiUrl+slackID)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         return data
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+// }
+
+// async function getSiegeUserDataOrCached(forceRefresh=false){
+//     let storedData=JSON.parse(localStorage.getItem("SiegeUserData"))
+//     if (forceRefresh || storedData==null || Date.now()>storedData.time+1800000){
+//         // get new data
+//         fetch(siegeUserApiUrl+slackID)
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error('Network response was not ok');
+//                 }
+//                 return response.json();
+//             })
+//             .then(data => {
+//                 return data
+//             })
+//             .catch(error => {
+//                 console.error('Error:', error);
+//             });
+//         localStorage.setItem("SiegeUserData",JSON.stringify({time:Date.now(), data:newData}))
+//         return newData
+//     }else{
+//     // use cached data
+//         console.log('using cached')
+//         return storedData.data
+//     }
+// }
+
+
+async function refreshProjectData(projectIDs){
+    let promises=projectIDs.map(projectID=>{
+        return [projectID,
+            fetch("https://siege.hackclub.com/api/public-beta/project/"+projectID)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+        ]
     })
-    .then(data => {
-        return data
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    projectsData={time:Date.now()}
+    console.log(promises)
+    for(promiseIdx in promises){
+        let promise=promises[promiseIdx]
+        projectsData[promise[0]]=await promise[1]
+        console.log(projectsData)
+    }
+    console.log("before return"+projectsData)
+    return projectsData
 }
 
-async function getSiegeUserDataOrCached(forceRefresh=false){
-    let storedData=JSON.parse(localStorage.getItem("SiegeUserData"))
-    if (forceRefresh || storedData==null || Date.now()>storedData.time+1800000){
-        // get new data
-        let newData=await (getSiegeUserData())
-        console.log("type:"+typeof newData)
-        localStorage.setItem("SiegeUserData",JSON.stringify({time:Date.now(), data:newData}))
-        return newData
-    }else{
-    // use cached data
-        console.log('using cached')
-        return storedData.data
+async function loadSiegeProjects(data){
+    console.log(data.id)
+    const siegeProjects=document.getElementById("siegeProjects")
+    siegeProjects.style.display='grid'
+    let storedData=JSON.parse(localStorage.getItem("SiegeProjectData"))
+    if(true||storedData==null || Date.now()>storedData.time+1800000){
+        projectIds=data.projects.map(project=>{return project.id})
+        // for(projectIdx in data.projects){
+        //     projectIds.push(data.projects[projectIdx].id)
+        // }
+        console.log(projectIds)
+        storedData=(await refreshProjectData(projectIds))
+        console.log(storedData)
+        localStorage.setItem("SiegeProjectData",JSON.stringify(storedData))
+    }
+    for (projectIdx in data.projects){
+        let project=data.projects[projectIdx]
+
+        const projectBox=document.createElement("div")
+        projectBox.classList.add('projectBox')
+        projectBox.innerHTML=
+        `
+        <h3>${project.name}</h3>
+        <p>${project.week_badge_text}</p>
+        <p>${storedData[project.id].description}</p>
+        <p>Status: ${project.status}</p>
+        `
+        siegeProjects.appendChild(projectBox)
     }
 }
 
 function loadSiegeStuff(){
-    getSiegeUserDataOrCached()
-        .then(data=>{
-            console.log(data)
-            console.log(data.id)
-            const siegeProjects=document.getElementById("siegeProjects")
-            siegeProjects.style.display='grid'
-            for (projectIdx in data.projects){
-                let project=data.projects[projectIdx]
-                const projectBox=document.createElement("div")
-                projectBox.classList.add('projectBox')
-                projectBox.innerHTML=
-                `
-                <h3>${project.name}</h3>
-                `
-                siegeProjects.appendChild(projectBox)
-            }
-        })
+    let storedData=JSON.parse(localStorage.getItem("SiegeUserData"))
+    if (storedData==null || Date.now()>storedData.time+1800000){
+        fetch(siegeUserApiUrl+slackID)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                localStorage.setItem("SiegeUserData",JSON.stringify({time:Date.now(), data:data}))
+                loadSiegeProjects(data)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }else{
+    // use cached data
+        console.log('using cached')
+        loadSiegeProjects(storedData.data)
+    }
 }
 
 function onload(){
